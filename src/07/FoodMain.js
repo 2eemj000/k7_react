086f4df16ea27252be60b055c2f103f711aec538
2eemj000 <167741157+2eemj000@users.noreply.github.com>
2024-05-13 09:19:13 +0900

    Update FoodMain.js

--- src/07/FoodMain.js
+++ src/07/FoodMain.js
@@ -32,7 +32,7 @@ export default function FoodMain() {
                         bcolor={'blue'}
                         handleClick={()=>{handleC1(item)}}/>
     )
-    const data = fooddata.map(item=>item);
+   // const data = fooddata.map(item=>item); -> 필요없음
     
 
   return (
